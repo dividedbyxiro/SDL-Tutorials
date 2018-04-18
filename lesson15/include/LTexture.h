f147700403954840b232fbae79db5d61e7f726fe
dividedbyxiro <dividedbyxiro@gmail.com>
2018-04-18 16:24:34 -0500

    completed lesson 15

---
+++ lesson15/include/LTexture.h
@@ -0,0 +1,27 @@
+#ifndef LTEXTURE_H
+#define LTEXTURE_H
+
+#include <string>
+#include <SDL.h>
+
+
+class LTexture
+{
+	public:
+		LTexture();
+		virtual ~LTexture();
+		void free();
+		bool loadFromFile(std::string path);
+		void render(int x, int y, SDL_Rect *source, double angle, SDL_Point *center, SDL_RendererFlip flip);
+		int getWidth();
+		int getHeight();
+
+	protected:
+
+	private:
+		SDL_Texture *mTexture;
+		int width;
+		int height;
+};
+
+#endif // LTEXTURE_H
